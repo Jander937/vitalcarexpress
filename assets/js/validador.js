b13d587635eba8cf5264e36db2768c12dc9f1bbf
Jhony Alexis Martinez <jhonyalexis100@gmail.com>
2023-06-23 09:49:41 -0500

    add ajustes

--- assets/js/validador.js
+++ assets/js/validador.js
@@ -83,7 +83,6 @@ button.addEventListener('click', function(evento) {
             document.getElementById('errorHora').textContent = 'El campo hora debe ser obligatorio';
          }
 
-
         
          if(errores.length == 0) {
             let datosEnvios = {
@@ -104,32 +103,3 @@ function validarEmail(email){
     const regularEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regularEmail.test(email)
 }
-
-
-        
-         if(errores.length == 0) {
-            let datosEnvios = {
-                usuario,
-                apellido,
-                documento,
-                email,
-                telefono,
-                fecha,
-                hora
-            }
-            citas.push(datosEnvios)
-            localStorage.setItem('datos', JSON.stringify(citas))
-         }   
-})
-
-function validarEmail(email){
-    const regularEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regularEmail.test(email)
-}
-
-
-
-
-
-    
-
